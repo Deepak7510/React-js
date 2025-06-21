abefc81d7ecc4582cf49fdb620ac02f17c0b2254
Deepak7510 <deepakkumaryadav75100@gmail.com>
2025-06-22 01:48:53 +0530

    first commit

--- basic/Code 2/App.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const h1 = React.createElement("h1", {}, "Hello");
-root.render(h1);
